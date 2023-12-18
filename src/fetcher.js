26e33f6344dfab19560f44b5ac21f18ec9b22bee
SKY <sky012877@gmail.com>
2023-12-18 09:42:09 -0800

    src/fetcher.js - Modified :

--- src/fetcher.js
+++ src/fetcher.js
@@ -18,3 +18,49 @@ export function fetchGet(endpointParams) {
             );
         });
 }
+
+export class WS extends WebSocket {
+    constructor(endpointParams, protocols) {
+        const url = new URL(endpointParams.url);
+        const params = new URLSearchParams(endpointParams.params);
+        url.search = params.toString();
+        super(url.toString(), protocols);
+
+        this.onopen = (event) => {
+            console.log("WebSocket connection opened:", event);
+            // Additional logic for when the WebSocket connection opens
+        };
+
+        // this.onmessage = (event) => {...
+        //     // console.log("Message received from server:", event.data);
+        //     // Additional logic for handling incoming messages
+        // };
+        //
+        this.onerror = (event) => {
+            console.error("WebSocket error:", event);
+            // Additional error handling logic
+        };
+
+        this.onclose = (event) => {
+            console.log("WebSocket connection closed:", event);
+            // Additional logic for when the WebSocket connection closes
+        };
+    }
+
+    sendToWS(message) {
+        const stringifiedMessage = JSON.stringify(message);
+
+        if (this.readyState === WebSocket.OPEN) {
+            this.send(stringifiedMessage);
+            return;
+        }
+
+        this.addEventListener(
+            "open",
+            () => {
+                this.send(stringifiedMessage);
+            },
+            { once: true }
+        );
+    }
+}
